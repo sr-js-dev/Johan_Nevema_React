412a2ad78a292c71d85db3c1e0ce290b845a0e64
madanodyblum <gorayblum@gmail.com>
2020-05-13 17:16:44 +0200

    last commit

--- src/pages/Signup/login.js
+++ src/pages/Signup/login.js
@@ -30,41 +30,6 @@ class Login extends React.Component {
   render() {
     
     return (
-      // <div className="auth-page" style={{height:"100%"}}>
-      //   <div className="container login-page">
-      //     <div className="row addQuestion">
-      //       <div className="col-md-5 offset-md-1 col-xs-12  vertical-center">
-      //           <Row style={{height:"100%",width:"100%"}}>
-      //             <div className="login-side-div">
-      //               <img src={require('../../assets/images/img_admin_side.png')} alt="appzmakerz" className="login-side-grad"></img>
-      //             </div>
-      //             <Col  className="login-form-div">
-      //               <img src={require('../../assets/images/appmakerz.svg')} alt="appzmakerz" style={{marginTop:"80px"}}></img>
-                    
-      //                <form className="login-form" onSubmit = { this.handleSubmit }>
-      //                <ListErrors errors={this.props.error} />
-      //                   <fieldset>  
-      //                       <fieldset className="form-group">
-      //                           <input type="text" name="username" className="orders__filters-search input-email" placeholder={trls("Username")}/>
-      //                       </fieldset>
-      //                       <fieldset className="form-group">
-      //                           <input type="password" name="password" className="orders__filters-search input-password" placeholder={trls("Password")}/>
-      //                       </fieldset>
-      //                       <p className="text-xs-center">
-      //                           <Link to="/register" style={{color:"rgb(84, 79, 79)"}}>
-      //                             {trls("Forgot_password")}
-      //                           </Link>
-      //                       </p>
-      //                       <button type="submit" className="btn-small place-and-orders__add-row-btn add-row sign-in">{trls("Sign_in")}</button>
-      //                   </fieldset>
-      //               </form>
-      //             </Col>
-      //           </Row>
-      //       </div>
-      //     </div>
-      //   </div>
-      //   <Pageloadspiiner/>
-      // </div>
       <div className="container auth-page">
         <div className="col-xl-5 col-lg-7 col-md-12  vertical-center">
             <Row>
@@ -78,13 +43,13 @@ class Login extends React.Component {
                 <Form className="container login-form" onSubmit = { this.handleSubmit }>
                     <Form.Group controlId="form">
                         <Col className="login-form__control">
-                          <Form.Control type="text" name="username" className="login-input-email" placeholder={trls("Email")}/>
+                          <Form.Control type="email" name="username" className="login-input-email" required placeholder={trls("Email")}/>
                           <label className="placeholder-label__login">{trls('Email')}</label>
                         </Col>
                     </Form.Group>
                     <Form.Group controlId="form">
                         <Col className="login-form__control">
-                          <Form.Control type="password" name="password" className="login-input-password" placeholder={trls("Password")}/>
+                          <Form.Control type="password" name="password" className="login-input-password" required placeholder={trls("Password")}/>
                           <label className="placeholder-label__login">{trls('Password')}</label>
                         </Col>
                     </Form.Group>
